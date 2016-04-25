e1cbb2331b8ac6391988a1a59c773689c662fa6e
Travis <travis.cox@unimelb.edu.au>
2016-04-25 14:58:57 +1000

    Playing with tilting

---
+++ Physics Puzzles/Assets/CreateSphereParticles.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateSphereParticles : MonoBehaviour {
+
+	public GameObject ObjectToInstantiate;
+	public int AmountToCreate = 10;
+	public float ChanceToInstantiate = 0.5f;
+
+
+	private int _instantiateCount = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_instantiateCount < AmountToCreate) {
+			if (Random.Range (0.00f, 1.00f) < ChanceToInstantiate) {
+				Instantiate (ObjectToInstantiate, transform.position, Quaternion.identity);
+				_instantiateCount++;
+			}
+		}
+	}
+}
---
+++ Physics Puzzles/Assets/ResizeLeg.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResizeLeg : MonoBehaviour {
+
+	private Vector3 startingScale;
+	private Vector3 newScale;
+
+	// Use this for initialization
+	void Start () {
+		startingScale = transform.localScale;
+		GetNewScale ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale = Vector3.Lerp (transform.localScale, newScale, 0.01f);
+		if (Vector3.Distance(transform.localScale, newScale) < 0.01) {
+			GetNewScale();
+		}
+	}
+
+	void GetNewScale(){
+		newScale = transform.localScale + new Vector3(0, Random.Range(-0.2f, 0.2f), 0);
+	}
+}
